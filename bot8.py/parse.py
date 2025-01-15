8d7025ced88c354bcc22bbaf5ee0b72300fd6337
YuSuF91132 <sabinader6@gmail.com>
2025-01-15 12:47:25 +0600

    parse.py

--- bot8.py/parse.py
+++ bot8.py/parse.py
@@ -1,13 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+count_news = 0
+
 def parse_news():
-    url = "https://m.akipress.org/"
-
+    count_news = 0
+    url = 'https://vesti.kg/'
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, "lxml")
+    soup = BeautifulSoup(response.text, 'lxml')
 
-    data = soup.find_all("div", class_="nowr_elem")
+    all_news = soup.find_all("div", class_="itemBody")[:5]
+    news_list = []
 
-    for d in data in range:   
-        news = d.find("a",class_="nowr_title" ).text
+    n ="""
+    """
+    for news in all_news:
+        count_news += 1
+        title = news.text.strip()
+        link = news.find("a")['href'] if news.find('a') else url
+        full_link = link if link.startswith("http") else f"https://vesti.kg/"
+        news_list.append(f"{count_news}: {title} {full_link}{n}")
+    return news_list    
